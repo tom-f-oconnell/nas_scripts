270c010de06cbe5f909eb24782f3b617c1425d1b
Tom O'Connell <toconnel@caltech.edu>
2020-01-04 19:07:43 -0800

    Initial commit. Function for sending gmail w/ app password.

---
+++ gmail_notifier.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+from os.path import dirname, realpath, join
+from email.mime.text import MIMEText
+import base64
+import smtplib
+
+
+def create_email(sender, to, subject, message_text):
+    """Create a message for an email.
+    Args:
+      sender: Email address of the sender.
+      to: Email address of the receiver.
+      subject: The subject of the email message.
+      message_text: The text of the email message.
+    Returns:
+      An object containing a base64url encoded email object.
+    """
+    message = MIMEText(message_text)
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
+    return {'raw': base64.urlsafe_b64encode(message.as_bytes()).decode()}
+
+
+def default_gmail_credential_path():
+    return join(dirname(realpath(__file__)), 'gmail_credentials.txt')
+
+
+def send_email(message, subject='', to=None, gmail_credential_file=None):
+    # See: https://support.google.com/accounts/answer/185833
+    # for how to generate "App Passwords", for offline access to gmail account
+    # (service accounts can't really be used with non-GSuit (i.e. personal)
+    # gmail accounts)
+    if gmail_credential_file is None:
+        gmail_credential_file = default_gmail_credential_path()
+
+    with open(gmail_credential_file, 'r') as f:
+        lines = [x for x in [x.strip() for x in f.readlines()] if x]
+    assert len(lines) == 2, f'got {len(lines)} lines, but expected 2'
+    gmail_acc, gmail_passw = lines
+    if '@' not in gmail_acc:
+        gmail_acc += '@gmail.com'
+
+    # Send to yourself by default.
+    if to is None:
+        to = gmail_acc
+
+    email = create_email(gmail_acc, to, subject, message)
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.ehlo()
+        server.login(gmail_acc, gmail_passw)
+        server.sendmail(gmail_acc, to, message)
+        server.close()
+        #print('Email sent!')
+    except Exception as err:
+        print('Something went wrong...')
+        #print(err)
+        #import ipdb; ipdb.set_trace()
+
+
+if __name__ == '__main__':
+    subject = 'Test subject'
+    message = 'Testing testing 123'
+    send_email(message, subject=subject)
+
